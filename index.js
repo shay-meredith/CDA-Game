9a11a854c829405daf0fbcbb4788a8540aa594e2
Shay Meredith <shay.e.meredith@gmail.com>
2017-12-10 16:45:15 -0500

    Add basic enemy animation

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function destroy() {
     const alien = document.getElementById('alien');
     alien.style.display = 'none';
 }
+
+window.onload = function run() {
+    const alien = document.getElementById('alien');
+    alien.classList.add('run');
+};
