42a746fb7e15d0bbd3fe1c767d21aa39eb883e88
Shay Meredith <shay.e.meredith@gmail.com>
2017-12-10 17:03:11 -0500

    Add start screen blur

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ function destroy() {
     alien.style.display = 'none';
 }
 
-window.onload = function run() {
+window.onload = function gameStart() {
     const alien = document.getElementById('alien');
     alien.classList.add('run');
+
+    const start = document.getElementsByClassName('play-container')[0];
+    start.classList.add('start-screen');
 };
